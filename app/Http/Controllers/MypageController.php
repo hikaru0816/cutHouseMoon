d0dd4c098096843a6fc883541719318342cf6014
hikaru0816 <woogamihikaru@gmail.com>
2023-01-25 17:17:15 +0900

    add checking my info

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 require(__DIR__ . '/../../../public/functions.php');
 
 use Illuminate\Http\Request;
--- resources/views/cutHouseMoon/mypage/mypage.blade.php
+++ resources/views/cutHouseMoon/mypage/mypage.blade.php
@@ -108,7 +108,43 @@
     <div class="wrapper">
         <h2>ご登録情報</h2>
         <div class="content-container">
-
+            <div class="confirm">
+                <div class="confirm-content">
+                    <div class="confirm-title">
+                        <p>ヨミ</p>
+                    </div>
+                    <div class="confirm-body">
+                        <p>{{ Auth::user()->kana }}</p>
+                    </div>
+                </div>
+                <div class="confirm-content">
+                    <div class="confirm-title">
+                        <p>名前</p>
+                    </div>
+                    <div class="confirm-body">
+                        <p>{{ Auth::user()->name }}</p>
+                    </div>
+                </div>
+                <div class="confirm-content">
+                    <div class="confirm-title">
+                        <p>電話番号</p>
+                    </div>
+                    <div class="confirm-body">
+                        <p>{{ Auth::user()->tel }}</p>
+                    </div>
+                </div>
+                <div class="confirm-content">
+                    <div class="confirm-title">
+                        <p>メールアドレス</p>
+                    </div>
+                    <div class="confirm-body">
+                        <p>{{ Auth::user()->email }}</p>
+                    </div>
+                </div>
+            </div>
+            <p class="rice-mark">
+                ※パスワードは個人情報保護のため表示しておりません。
+            </p>
         </div>
     </div>
 </section>
