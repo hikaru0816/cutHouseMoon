6af78b470d71f38a41d3a4581416f505042cfbe2
hikaru0816 <woogamihikaru@gmail.com>
2023-01-27 13:12:37 +0900

    add UsersTableSeeder

--- database/seeds/EmptyStatusesTableSeeder.php
+++ database/seeds/EmptyStatusesTableSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class EmptyStatuesTableSeeder extends Seeder
+class EmptyStatusesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
--- database/seeds/WaitingStatusesTableSeeder.php
+++ database/seeds/WaitingStatusesTableSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class WaitingStatuesTableSeeder extends Seeder
+class WaitingStatusesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
